18fe362f45e62a83503e88a995dd0fb99da05ba2
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-07 08:59:58 +0100

    chore: rename to avoid name clashing

--- src/llmling_models/__init__.py
+++ src/llmling_models/__init__.py
@@ -6,7 +6,7 @@ from llmling_models.multi import MultiModel
 from llmling_models.inputmodel import InputModel
 from llmling_models.importmodel import ImportModel
 from llmling_models.multimodels.fallback import FallbackMultiModel
-from llmling_models.multimodels.token import TokenOptimizedMultiModel
+from llmling_models.multimodels.token_optimized import TokenOptimizedMultiModel
 from llmling_models.multimodels.cost import CostOptimizedMultiModel
 from llmling_models.multimodels.delegation import DelegationMultiModel
 
--- src/llmling_models/multimodels/__init__.py
+++ src/llmling_models/multimodels/__init__.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 from llmling_models.multimodels.fallback import FallbackMultiModel
 from llmling_models.multimodels.delegation import DelegationMultiModel
 from llmling_models.multimodels.cost import CostOptimizedMultiModel
-from llmling_models.multimodels.token import TokenOptimizedMultiModel
+from llmling_models.multimodels.token_optimized import TokenOptimizedMultiModel
 
 __all__ = [
     "CostOptimizedMultiModel",
