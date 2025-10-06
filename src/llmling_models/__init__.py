5dd7362636fa66d9e32cfd2c7cfb774f59430adb
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 06:47:12 +0200

    chore: fix init file

--- src/llmling_models/__init__.py
+++ src/llmling_models/__init__.py
@@ -2,4 +2,31 @@ from __future__ import annotations
 
 from importlib.metadata import version
 
+from llmling_models.base import PydanticModel
+from llmling_models.multi import MultiModel
+from llmling_models.inputmodel import InputModel
+from llmling_models.input_handlers import DefaultInputHandler
+from llmling_models.multimodels import (
+    TokenOptimizedMultiModel,
+    CostOptimizedMultiModel,
+    DelegationMultiModel,
+    UserSelectModel,
+)
+from llmling_models.utils import infer_model
+from llmling_models.model_types import AllModels, ModelInput
+
 __version__ = version("llmling-models")
+
+__all__ = [
+    "AllModels",
+    "CostOptimizedMultiModel",
+    "DefaultInputHandler",
+    "DelegationMultiModel",
+    "InputModel",
+    "ModelInput",
+    "MultiModel",
+    "PydanticModel",
+    "TokenOptimizedMultiModel",
+    "UserSelectModel",
+    "infer_model",
+]
