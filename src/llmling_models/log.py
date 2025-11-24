b09ac81a2c4955fbb8c60cb14eed6fbe6df60771
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-24 12:45:09 +0100

    chore: copier update

--- src/llmling_models/log.py
+++ src/llmling_models/log.py
@@ -5,13 +5,21 @@ from __future__ import annotations
 import logging
 
 
-def get_logger(name: str) -> logging.Logger:
+LogLevel = int | str
+
+
+def get_logger(name: str, log_level: LogLevel | None = None) -> logging.Logger:
     """Get a logger for the given name.
 
     Args:
-        name: The name of the logger, will be prefixed with 'llmling_agent.'
+        name: The name of the logger.
+              Will be prefixed with 'llmling_models'
+        log_level: The logging level to set for the logger
 
     Returns:
         A logger instance
     """
-    return logging.getLogger(f"llmling_models.{name}")
+    logger = logging.getLogger(f"llmling_models.{name}")
+    if log_level is not None:
+        logger.setLevel(log_level)
+    return logger
